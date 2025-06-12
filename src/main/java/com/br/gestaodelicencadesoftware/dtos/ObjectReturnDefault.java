fcd1925c027ab9ecf1187d52604542aedb00ddd7
bruno <brunofsantosdev@gmail.com>
2025-06-12 20:55:15 -0300

    Testes de unidade para supplier

--- src/main/java/com/br/gestaodelicencadesoftware/dtos/ObjectReturnDefault.java
+++ src/main/java/com/br/gestaodelicencadesoftware/dtos/ObjectReturnDefault.java
@@ -1,7 +1,26 @@
 package com.br.gestaodelicencadesoftware.dtos;
 
-public record ObjectReturnDefault(String message, Object object) {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class ObjectReturnDefault implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String message;
+    private Object object;
+
     public ObjectReturnDefault(String message) {
-        this(message, null);
+        this.message = message;
+    }
+
+    public ObjectReturnDefault(String message, Object object) {
+        this.message = message;
+        this.object = object;
     }
 }
